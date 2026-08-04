486bf8e930d7633e5cb6c284bbba4991dab2f774
iyiu9akhan <iyiu9akhan@gmail.com>
2026-08-04 22:33:33 +0600

    redux localHost data handling succuessfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
-import React, { useState } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+// import { Navigate } from 'react-router-dom'
 
 function home() {
+    const auth = getAuth();
+    const navigate = useNavigate()
+    const data = useSelector(state => state.userInfo.value)
     const [verify, setVerify] = useState(false)
+    console.log(data, "data")
+
+    useEffect(() => {
+        if (!data) {
+            navigate("/login")
+        }
+    }, [])
+
+    onAuthStateChanged(auth, (user) => {
+        console.log(user, "home user")
+        if (user.emailVerified) {
+            setVerify(true)
+        }
+    });
 
     return (
         <>
@@ -9,7 +30,7 @@ function home() {
                 verify ?
                     <div>Lorem ipsum dolor sit amet.</div>
                     :
-                    <p>verify ur main</p>
+                    <p>verify ur gmail</p>
             }
         </>
     )
