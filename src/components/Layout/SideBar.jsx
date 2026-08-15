dc197e0bab784e06564451fc295aeecd3150dbe9
iyiu9akhan <iyiu9akhan@gmail.com>
2026-08-15 21:21:14 +0600

    shadCN issue

--- src/components/Layout/SideBar.jsx
+++ src/components/Layout/SideBar.jsx
@@ -1,30 +1,18 @@
 import React from 'react'
-import Container from './Container'
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import Container from './Container'
 
 function SideBar() {
   return (
-    <>
-      <Container>
-        <SidebarProvider>
-          <Sidebar>
-            <SidebarHeader />
-            <SidebarContent>
-              <SidebarGroup />
-              <SidebarGroup />
-            </SidebarContent>
-            <SidebarFooter />
-          </Sidebar>
-        </SidebarProvider>
-      </Container>
-    </>
+    <Container>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        </main>
+      </SidebarProvider>
+    </Container>
   )
 }
 
