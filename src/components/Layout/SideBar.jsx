cd53e73570ad7451ee4af045b0660aa62827cefc
iyiu9akhan <iyiu9akhan@gmail.com>
2026-08-17 21:54:10 +0600

    shadCN issue fixed

--- src/components/Layout/SideBar.jsx
+++ src/components/Layout/SideBar.jsx
@@ -3,13 +3,14 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import Container from './Container'
 
-function SideBar() {
+function SideBar({children}) {
   return (
     <Container>
       <SidebarProvider>
         <AppSidebar />
         <main>
           <SidebarTrigger />
+          {children}
         </main>
       </SidebarProvider>
     </Container>
